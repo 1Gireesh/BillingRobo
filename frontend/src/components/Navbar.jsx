a5526e5c897fa1160b3e3519c7c1b092666d40d7
1Gireesh <gireeshbhat68@gmail.com>
2022-11-30 11:53:23 +0530

    added react-router chakra

---
+++ frontend/src/components/Navbar.jsx
@@ -0,0 +1,12 @@
+import { Box, Button } from '@chakra-ui/react';
+import React from 'react';
+
+const Navbar = () => {
+    return (
+        <Box>
+            <Button></Button>
+        </Box>
+    );
+}
+
+export default Navbar;
